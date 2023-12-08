88147ab9310bab740171990bc766343a86454c91
Hugo <hugo@MacBook-Pro-de-Hugo.local>
2023-12-08 17:00:05 +0100

    Ajouté des modales

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { StatusBar, SafeAreaView, StyleSheet, Text, Keyboard, KeyboardAvoidingView, Alert, ImageBackground } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet, Text, Keyboard, KeyboardAvoidingView, ImageBackground, 
+  Modal, TextInput, TouchableOpacity, View
+} from 'react-native';
 import GoalList from './GoalList';
 import GoalInput from './GoalInput';
 
@@ -17,52 +19,59 @@ const App = () => {
     "Avoir une meuf"
   ]);
 
-  const deleteGoal = (index) => {
-    Alert.alert(
-      "Supprimer l'objectif",
-      'Voulez-vous vraiment supprimer cet objectif ?',
-      [
-        {
-          text: 'Annuler',
-          style: 'cancel',
-        },
-        {
-          text: 'Supprimer',
-          onPress: () => {
-            setSampleGoals((goals) => {
-              const updatedGoals = [...goals];
-              updatedGoals.splice(index, 1);
-              return updatedGoals;
-            });
-          },
-        },
-      ],
-    );
+  const [isModifyModalVisible, setModifyModalVisible] = useState(false);
+  const [modifyText, setModifyText] = useState('');
+  const [modifyIndex, setModifyIndex] = useState(null);
+
+  const [isDeleteModalVisible, setDeleteModalVisible] = useState(false);
+  const [deleteIndex, setDeleteIndex] = useState(null);
+
+  const openModifyModal = (index) => {
+    setModifyText(sampleGoals[index]);
+    setModifyIndex(index);
+    setModifyModalVisible(true);
+  };
+
+  const closeModifyModal = () => {
+    setModifyText('');
+    setModifyIndex(null);
+    setModifyModalVisible(false);
+  };
+
+  const updateGoal = () => {
+    if (modifyIndex !== null) {
+      setSampleGoals((goals) => {
+        const updatedGoals = [...goals];
+        updatedGoals[modifyIndex] = modifyText.trim();
+        return updatedGoals;
+      });
+    }
+    closeModifyModal();
+  };
+
+  const openDeleteModal = (index) => {
+    setDeleteIndex(index);
+    setDeleteModalVisible(true);
+  };
+
+  const closeDeleteModal = () => {
+    setDeleteIndex(null);
+    setDeleteModalVisible(false);
+  };
+
+  const deleteGoal = () => {
+    if (deleteIndex !== null) {
+      setSampleGoals((goals) => {
+        const updatedGoals = [...goals];
+        updatedGoals.splice(deleteIndex, 1);
+        return updatedGoals;
+      });
+    }
+    closeDeleteModal();
   };
 
   const modifyGoal = (index) => {
-    Alert.prompt(
-      "Modifier l'objectif",
-      'Modifiez votre objectif :',
-      [
-        {
-          text: 'Annuler',
-          style: 'cancel',
-        },
-        {
-          text: 'Modifier',
-          onPress: (newGoal) => {
-            setSampleGoals((goals) => {
-              const updatedGoals = [...goals];
-              updatedGoals[index] = newGoal.trim();
-              return updatedGoals;
-            });
-          },
-        },
-      ],
-      'plain-text',
-      sampleGoals[index] 
-    );
+    openModifyModal(index);
   };
 
   const addGoal = (newGoal) => {
@@ -82,9 +91,76 @@ const App = () => {
         <SafeAreaView style={styles.container}>
           <Text style={styles.title}>Objectifs</Text>
 
-          <GoalList goals={sampleGoals} onDelete={deleteGoal} onModify={modifyGoal}/>
+          <GoalList
+            goals={sampleGoals}
+            onDelete={(index) => openDeleteModal(index)}
+            onModify={modifyGoal}
+          />
 
           <GoalInput onAdd={addGoal} />
+
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={isModifyModalVisible}
+          >
+            <View style={styles.modifyModalContainer}>
+              <View style={styles.modifyModalContent}>
+                <Text style={styles.modalText}>Modifier l'objectif</Text>
+                <Text style={styles.confirmationText}>
+                  Souhaitez-vous modifier "{sampleGoals[modifyIndex]}" ?
+                </Text>
+                <TextInput
+                  style={styles.input}
+                  value={modifyText}
+                  onChangeText={(text) => setModifyText(text)}
+                />
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity
+                    style={[styles.button, styles.cancelButton]}
+                    onPress={closeModifyModal}
+                  >
+                    <Text style={styles.buttonText}>Annuler</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={[styles.button, styles.validateButton]}
+                    onPress={updateGoal}
+                  >
+                    <Text style={styles.buttonText}>Modifier</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={isDeleteModalVisible}
+          >
+            <View style={styles.modifyModalContainer}>
+              <View style={styles.modifyModalContent}>
+                <Text style={styles.modalText}>Supprimer l'objectif</Text>
+                <Text style={styles.confirmationText}>
+                  Souhaitez-vous supprimer "{sampleGoals[deleteIndex]}" ?
+                </Text>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity
+                    style={[styles.button, styles.cancelButton]}
+                    onPress={closeDeleteModal}
+                  >
+                    <Text style={styles.buttonText}>Annuler</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={[styles.button, styles.validateButton]}
+                    onPress={deleteGoal}
+                  >
+                    <Text style={styles.buttonText}>Supprimer</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
 
           <StatusBar style="auto" />
         </SafeAreaView>
@@ -106,6 +182,64 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex: 1,
+  },
+  modifyModalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modifyModalContent: {
+    backgroundColor: 'black',
+    padding: 20,
+    width: 300,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    color: 'white',
+    marginBottom: 20,
+    fontWeight: 'bold',
+    fontSize: 25
+  },
+  goalText: {
+    color: 'white',
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  confirmationText: {
+    color: 'white',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
+    color: 'white',
+    width: '80%',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  button: {
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  cancelButton: {
+    backgroundColor: 'red',
+  },
+  validateButton:{
+    backgroundColor:'#2ecc71',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign:'center',
+    width: 80
   },
 });
 
--- GoalInput.js
+++ GoalInput.js
@@ -1,37 +1,27 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableHighlight, StyleSheet, Alert, Keyboard } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Modal } from 'react-native';
 
 const GoalInput = ({ onAdd }) => {
   const [newGoal, setNewGoal] = useState('');
+  const [isAddModalVisible, setAddModalVisible] = useState(false);
 
-  const showAlert = () => {
-    Keyboard.dismiss();
-    Alert.alert(
-      "Confirmer l'ajout",
-      `Voulez-vous vraiment ajouter l'objectif "${newGoal.trim()}" ?`,
-      [
-        {
-          text: 'Annuler',
-          style: 'cancel',
-          onPress: () => {
-            setNewGoal('');
-          },
-        },
-        {
-          text: 'Ajouter',
-          onPress: addNewGoal,
-          disabled: newGoal.trim() === '',
-        },
-      ],
-    );
+  const openAddModal = () => {
+    setAddModalVisible(true);
+  };
+
+  const closeAddModal = () => {
+    setAddModalVisible(false);
+    setNewGoal('');
   };
 
   const addNewGoal = () => {
     if (newGoal.trim() !== '') {
       onAdd(newGoal.trim());
-      setNewGoal('');
+      closeAddModal();
     }
   };
+
+  const isAddButtonDisabled = newGoal.trim() === '';
 
   return (
     <View style={styles.inputContainer}>
@@ -42,14 +32,43 @@ const GoalInput = ({ onAdd }) => {
         value={newGoal}
         onChangeText={(text) => setNewGoal(text)}
       />
-      <TouchableHighlight
-        underlayColor="#2674c2"
-        onPress={showAlert}
-        style={[styles.button, newGoal.trim() === '' && styles.disabledButton]}
-        disabled={newGoal.trim() === ''}
+      <TouchableOpacity onPress={openAddModal} disabled={isAddButtonDisabled}>
+        <View style={[ styles.button, isAddButtonDisabled && styles.disabledButton]}>
+          <Text style={styles.buttonText}>Ajouter</Text>
+        </View>
+      </TouchableOpacity>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isAddModalVisible}
       >
-        <Text style={styles.buttonText}>Ajouter</Text>
-      </TouchableHighlight>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>
+              Ajouter un objectif
+            </Text>
+            <Text style={styles.objectifText}>
+                  Souhaitez-vous ajouter "{newGoal.trim()}" ?
+            </Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={[styles.button, styles.cancelButton]}
+                onPress={closeAddModal}
+              >
+                <Text style={styles.buttonText}>Annuler</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: '#2ecc71' }]}
+                onPress={addNewGoal}
+                disabled={isAddButtonDisabled}
+              >
+                <Text style={styles.buttonText}>Ajouter</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -68,26 +87,54 @@ const styles = StyleSheet.create({
     color: 'white',
     marginRight: 10,
     marginLeft: 10,
+    borderRadius: 10
   },
   button: {
-    borderRadius: 5,
+    borderRadius: 10, 
     paddingVertical: 10,
     paddingHorizontal: 20,
     marginRight: 10,
     backgroundColor: '#3498db',
-  },
-  disabledButton: {
-    backgroundColor: 'gray',
+    color: 'white',
+    fontSize: 16,
   },
   buttonText: {
     color: 'white',
     fontSize: 16,
   },
-  placeholderText: {
-    position: 'absolute',
-    top: 10,
-    left: 20,
+  disabledButton: {
+    backgroundColor: 'gray',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'black',
+    padding: 20,
+    width: 300,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
     color: 'white',
+    marginBottom: 10,
+    fontSize:20
+  },
+  objectifText: {
+    color: 'white',
+    marginBottom: 20,
+    fontSize: 13,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  cancelButton: {
+    backgroundColor: 'red',
   },
 });
 
